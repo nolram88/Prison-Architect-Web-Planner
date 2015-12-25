a8c9d69ac41cea7a4a1cb286ad711e969a6aa6b4
marlonmckinnie <marlonmckinnie@gmail.com>
2015-12-24 22:27:05 -0500

    more server like stuff

--- app/main.js
+++ app/main.js
@@ -4,6 +4,14 @@ var app = express();
 app.get('/', function (req, res) {
   res.redirect('/index.html');
 });
+
+app.get('/_ah/health', function(req, res) {
+  res.status(200).send('ok');
+});
+app.use(function(err, req, res, next) {
+  /* jshint unused:false */
+  res.status(500).send('Something broke!');
+});
 app.use(express.static('app'));
 var server = app.listen(8080, function () {
   var host = server.address().address;
